f5af3fd0d71099613da4f16ecb8849fe55b1d3f2
Andrew Scholer <ascholer@chemeketa.edu>
2023-12-03 16:32:34 -0800

    Template

---
+++ template/index.js
@@ -0,0 +1,18 @@
+import { getData } from "utils";
+
+const lines = getData("data.txt").split("\n");
+
+function part1() {
+  let sum = 0;
+
+  console.log(sum);
+}
+
+function part2() {
+  let sum = 0;
+
+  console.log(sum);
+}
+
+part1();
+part2();
