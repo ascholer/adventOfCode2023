0a3c3810815b1f74389322273fbb9c95902955e2
Andrew Scholer <ascholer@chemeketa.edu>
2023-12-05 08:23:40 -0800

    Add chunks to utils

--- utils/index.mjs
+++ utils/index.mjs
@@ -10,4 +10,12 @@ function getData(filename) {
   }
 }
 
-export { getData };
+function chunks(arr, n) {
+  let result = [];
+  for (let i = 0; i < arr.length; i += n) {
+    result.push(arr.slice(i, i + n));
+  }
+  return result;
+}
+
+export { getData, chunks };
